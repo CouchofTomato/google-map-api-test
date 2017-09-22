4e570add97f4ed723bab90a5ebabb2214f008cbc
Austin Mason <austinpmason@gmail.com>
2017-09-22 14:05:26 +0000

    finished

---
+++ README.md
@@ -0,0 +1,5 @@
+Project taken from The Odin Project
+
+https://www.theodinproject.com/courses/javascript-and-jquery/lessons/putting-google-maps-onto-your-site
+
+Join me and learn to code
---
+++ js/main.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const long = document.getElementById('longitude')
+const lat = document.getElementById('latitude')
+const messageBox = document.getElementById('message')
+const submit = document.getElementById('submit')
+let mapBox = document.getElementById('map')
+const mapProps = {
+  center: { lat: 51.508742, lng: -0.120850 },
+  zoom: 8
+}
+let map
+
+function initMap() {
+  map = new google.maps.Map(mapBox, mapProps)
+}
+
+const resetInputs = () => {
+  long.value = ''
+  lat.value = ''
+  messageBox.value = ''
+}
+
+const addMarkerCallback = (message, marker) => {
+  google.maps.event.addListener(marker, 'click', function() {
+    var infowindow = new google.maps.InfoWindow({
+      content: message
+    });
+    infowindow.open(map, marker);
+  });
+}
+
+const addMessage = (e) => {
+  e.preventDefault()
+  const message = messageBox.value
+  const myCenter = new google.maps.LatLng(lat.value, long.value)
+  const marker = new google.maps.Marker({ position: myCenter })
+  marker.setMap(map)
+  addMarkerCallback(message, marker)
+  resetInputs()
+}
+
+submit.addEventListener('click', addMessage, false)
